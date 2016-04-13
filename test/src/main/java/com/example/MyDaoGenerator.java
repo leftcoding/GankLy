036b42e5f5bcb8e017e50f9aced59a1e2e9c3e24
leftcoding <137387869@qq.com>
2016-04-13 09:28:49 +0800

    GreenDao数据库

---
+++ test/src/main/java/com/example/MyDaoGenerator.java
@@ -0,0 +1,46 @@
+package com.example;
+
+import de.greenrobot.daogenerator.DaoGenerator;
+import de.greenrobot.daogenerator.Entity;
+import de.greenrobot.daogenerator.Property;
+import de.greenrobot.daogenerator.Schema;
+import de.greenrobot.daogenerator.ToMany;
+
+/**
+ * Create by LingYan on 2016-04-11
+ */
+public class MyDaoGenerator {
+    public static void main(String[] args) throws Exception {
+        Schema schema = new Schema(1, "com.gank.gankly");
+
+        addNote(schema);
+        addCustomerOrder(schema);
+
+        new DaoGenerator().generateAll(schema, "E:/GankLy/app/src/main/java");
+    }
+
+    private static void addNote(Schema schema) {
+        Entity note = schema.addEntity("UrlCollect");//类名
+        note.addIdProperty();
+        note.addStringProperty("url").notNull();
+        note.addStringProperty("comment");
+        note.addDateProperty("date");
+    }
+
+    private static void addCustomerOrder(Schema schema) {
+        Entity customer = schema.addEntity("Customer");
+        customer.addIdProperty();
+        customer.addStringProperty("name").notNull();
+
+        Entity order = schema.addEntity("Order");
+        order.setTableName("ORDERS"); // "ORDER" is a reserved keyword
+        order.addIdProperty();
+        Property orderDate = order.addDateProperty("date").getProperty();
+        Property customerId = order.addLongProperty("customerId").notNull().getProperty();
+        order.addToOne(customer, customerId);
+
+        ToMany customerToOrders = customer.addToMany(order, customerId);
+        customerToOrders.setName("orders");
+        customerToOrders.orderAsc(orderDate);
+    }
+}
