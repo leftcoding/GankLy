0e49037b80a531367f1af6a05457eacebc043d54
leftcoding <137387869@qq.com>
2016-05-17 14:27:23 +0800

    1. 添加图片分享到 功能

--- app/src/main/java/com/gank/gankly/utils/ShareUtils.java
+++ app/src/main/java/com/gank/gankly/utils/ShareUtils.java
@@ -3,15 +3,9 @@ package com.gank.gankly.utils;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
-import android.os.AsyncTask;
 import android.text.TextUtils;
 
-import com.bumptech.glide.Glide;
-import com.bumptech.glide.request.target.Target;
 import com.gank.gankly.R;
-import com.socks.library.KLog;
-
-import java.io.File;
 
 /**
  * Create by LingYan on 2016-04-22
@@ -25,18 +19,6 @@ public class ShareUtils {
         }
         return mShareUtils;
     }
-
-//    //分享单张图片
-//    public void shareSingleImage(Context context, Uri imageUri) {
-//        if (imageUri == null) {
-//            return;
-//        }
-//        Intent shareIntent = new Intent();
-//        shareIntent.setAction(Intent.ACTION_SEND);
-//        shareIntent.putExtra(Intent.EXTRA_STREAM, imageUri);
-//        shareIntent.setType("image/*");
-//        context.startActivity(Intent.createChooser(shareIntent, "分享到"));
-//    }
 
     //分享文字
     public void shareText(Context context, String share) {
@@ -54,49 +36,6 @@ public class ShareUtils {
         shareIntent.putExtra(Intent.EXTRA_TEXT, share);
         shareIntent.setType("text/plain");
         context.startActivity(Intent.createChooser(shareIntent, "分享到"));
-    }
-
-    public class ShareTask extends AsyncTask<String, Void, File> {
-        private final Context context;
-
-        public ShareTask(Context context) {
-            this.context = context;
-        }
-
-        @Override
-        protected File doInBackground(String... params) {
-            String url = params[0]; // should be easy to extend to share multiple images at once
-            try {
-                return Glide
-                        .with(context)
-                        .load(url)
-                        .downloadOnly(Target.SIZE_ORIGINAL, Target.SIZE_ORIGINAL)
-                        .get();
-            } catch (Exception ex) {
-                KLog.e("SHARE", "Sharing " + url + " failed", ex);
-                return null;
-            }
-        }
-
-        @Override
-        protected void onPostExecute(File result) {
-            if (result == null) {
-                return;
-            }
-            Uri uri = Uri.fromFile(result);
-            share(uri);
-        }
-
-        private void share(Uri result) {
-            Intent intent = new Intent(Intent.ACTION_SEND);
-            intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION); // temp permission for receiving app to read this file
-            intent.setType("image/*");
-            intent.putExtra(Intent.EXTRA_SUBJECT, "分享图片");
-            intent.putExtra(Intent.EXTRA_TEXT, "Look what I found!");
-            intent.putExtra(Intent.EXTRA_STREAM, result);
-            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            context.startActivity(Intent.createChooser(intent, "分享图片"));
-        }
     }
 
     public static void shareSingleImage(Context context, Uri result) {
