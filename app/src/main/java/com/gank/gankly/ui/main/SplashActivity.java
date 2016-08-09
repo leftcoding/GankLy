681beae2d2d63036ed5bdc3e7884ef54a309b0cd
leftcoding <137387869@qq.com>
2016-08-09 17:21:53 +0800

    1.Update SplashActivity background

--- app/src/main/java/com/gank/gankly/ui/main/SplashActivity.java
+++ app/src/main/java/com/gank/gankly/ui/main/SplashActivity.java
@@ -1,19 +1,31 @@
 package com.gank.gankly.ui.main;
 
+import android.animation.Animator;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.annotation.Nullable;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 
 import com.gank.gankly.R;
 import com.gank.gankly.ui.base.BaseActivity;
 
+import java.util.concurrent.TimeUnit;
+
+import butterknife.Bind;
+import rx.Observable;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Action1;
+
 /**
  * Create by LingYan on 2016-06-01
  */
 public class SplashActivity extends BaseActivity {
+    @Bind(R.id.splash_main)
+    View mView;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -44,14 +56,46 @@ public class SplashActivity extends BaseActivity {
 
     @Override
     protected void initValues() {
-        new Handler().postDelayed(new Runnable() {
+        Observable.timer(500, TimeUnit.MILLISECONDS)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Action1<Long>() {
+                    @Override
+                    public void call(Long aLong) {
+                        startAnim();
+                    }
+                });
+    }
+
+    private void startAnim() {
+        ObjectAnimator objectAnimatorX = ObjectAnimator.ofFloat(mView, "scaleX", 1f, 1.5f);
+        ObjectAnimator objectAnimatorY = ObjectAnimator.ofFloat(mView, "scaleY", 1f, 1.5f);
+        AnimatorSet set = new AnimatorSet();
+        set.setDuration(1000).playTogether(objectAnimatorX, objectAnimatorY);
+        set.addListener(new Animator.AnimatorListener() {
             @Override
-            public void run() {
+            public void onAnimationStart(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
                 Intent intent = new Intent(SplashActivity.this, MainActivity.class);
                 startActivity(intent);
+                SplashActivity.this.overridePendingTransition(R.anim.alpha_in, R.anim.alpha_out);
                 finish();
             }
-        }, 500);
+
+            @Override
+            public void onAnimationCancel(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animation) {
+
+            }
+        });
+        set.start();
     }
 
     @Override
