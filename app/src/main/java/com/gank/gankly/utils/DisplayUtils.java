9076fdd0fb922201ce9c15e6f3b34908019614a4
leftcoding <137387869@qq.com>
2016-08-23 15:29:13 +0800

    1.Add DisplayUtils

---
+++ app/src/main/java/com/gank/gankly/utils/DisplayUtils.java
@@ -0,0 +1,42 @@
+/**
+ * DisplayUtils.java
+ * <p/>
+ * Created by xuanzhui on 2015/9/10.
+ * Copyright (c) 2015 BeeCloud. All rights reserved.
+ */
+package com.gank.gankly.utils;
+
+import android.content.Context;
+
+public class DisplayUtils {
+
+    public static float px2dp(final Context context, final float px) {
+        return px / context.getResources().getDisplayMetrics().density;
+    }
+
+    public static float dp2px(final Context context, final float dp) {
+        return dp * context.getResources().getDisplayMetrics().density;
+    }
+
+    /**
+     * 将px值转换为sp值，保证文字大小不变
+     *
+     * @param pxValue
+     * @return
+     */
+    public static int px2sp(Context context, float pxValue) {
+        final float fontScale = context.getResources().getDisplayMetrics().scaledDensity;
+        return (int) (pxValue / fontScale + 0.5f);
+    }
+
+    /**
+     * 将sp值转换为px值，保证文字大小不变
+     *
+     * @param spValue
+     * @return
+     */
+    public static int sp2px(Context context, float spValue) {
+        final float fontScale = context.getResources().getDisplayMetrics().scaledDensity;
+        return (int) (spValue * fontScale + 0.5f);
+    }
+}
