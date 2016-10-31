f1cba58c48f7ab5c343cb74220d3965c125e3063
leftcoding <137387869@qq.com>
2016-10-31 17:14:58 +0800

    1.Update MVP 2.Add History

---
+++ app/src/main/java/com/gank/gankly/ui/history/BrowseHistoryContract.java
@@ -0,0 +1,9 @@
+package com.gank.gankly.ui.history;
+
+/**
+ * Create by LingYan on 2016-10-31
+ * Email:137387869@qq.com
+ */
+
+public interface BrowseHistoryContract {
+}
---
+++ app/src/main/java/com/gank/gankly/ui/history/BrowseHistoryFragment.java
@@ -0,0 +1,35 @@
+package com.gank.gankly.ui.history;
+
+import com.gank.gankly.mvp.base.FetchFragment;
+
+/**
+ * Create by LingYan on 2016-10-31
+ * Email:137387869@qq.com
+ */
+
+public class BrowseHistoryFragment extends FetchFragment {
+    @Override
+    protected void initPresenter() {
+
+    }
+
+    @Override
+    protected void initValues() {
+
+    }
+
+    @Override
+    protected void initViews() {
+
+    }
+
+    @Override
+    protected void bindLister() {
+
+    }
+
+    @Override
+    protected int getLayoutId() {
+        return 0;
+    }
+}
