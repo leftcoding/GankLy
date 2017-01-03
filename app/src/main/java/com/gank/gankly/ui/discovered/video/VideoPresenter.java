4b411a8f75590b7b10fcedbbbfa418e6185755af
leftcoding <137387869@qq.com>
2017-01-03 17:24:16 +0800

    1.Update lambda

--- app/src/main/java/com/gank/gankly/ui/discovered/video/VideoPresenter.java
+++ app/src/main/java/com/gank/gankly/ui/discovered/video/VideoPresenter.java
@@ -1,9 +1,11 @@
 package com.gank.gankly.ui.discovered.video;
 
+import com.gank.gankly.mvp.FetchPresenter;
+
 /**
  * Create by LingYan on 2017-01-03
  * Email:137387869@qq.com
  */
 
-public class VideoPresenter {
+public class VideoPresenter extends FetchPresenter {
 }
